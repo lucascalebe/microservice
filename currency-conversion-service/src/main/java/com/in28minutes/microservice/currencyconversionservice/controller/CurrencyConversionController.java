1a609dcc45584ee1763515314e919cd028cf7a48
Lucas Calebe <lucascalebe07@hotmail.com>
2022-07-09 19:39:50 -0300

    :sparkles: invoking currency exchange from currency conversion with rest template

--- currency-conversion-service/src/main/java/com/in28minutes/microservice/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservice/currencyconversionservice/controller/CurrencyConversionController.java
@@ -4,8 +4,10 @@ import com.in28minutes.microservice.currencyconversionservice.model.CurrencyCove
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
 @RestController
 public class CurrencyConversionController {
@@ -14,6 +16,18 @@ public class CurrencyConversionController {
   public CurrencyCoversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to,
                                                        @PathVariable BigDecimal quantity) {
 
-    return new CurrencyCoversion(10001L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "");
+    HashMap<String, String> uriVariables = new HashMap<>();
+    uriVariables.put("from", from);
+    uriVariables.put("to", to);
+
+    var currencyConversion = new RestTemplate().getForEntity(
+            "http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+            CurrencyCoversion.class, uriVariables).getBody();
+
+    return new CurrencyCoversion(currencyConversion.getId(),from, to, quantity,
+            currencyConversion.getConversionMultiple(),
+            quantity.multiply(currencyConversion.getConversionMultiple()),
+            currencyConversion.getEnvironment());
+
   }
 }
