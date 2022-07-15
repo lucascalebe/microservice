137aeb2eba47f5339e2ff40d40ce8a95d28bc468
Lucas Calebe <lucascalebe07@hotmail.com>
2022-07-15 11:13:09 -0300

    :tada: create naming-server project

---
+++ naming-server/src/test/java/com/in28minutes/microservice/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservice.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
